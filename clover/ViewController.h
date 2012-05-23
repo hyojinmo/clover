63b2eeb6b4f4ae55186ef2fe2ae0df4074fe322d
dongkang <dongkang0626@daum.net>
2012-05-23 12:44:29 +0900

    기본적인 프로젝트 구조로 셋팅

--- clover/ViewController.h
+++ clover/ViewController.h
@@ -1,17 +1,8 @@
-//
-//  ViewController.h
-//  cloverTest2
-//
-//  Created by 강동혁 on 12. 5. 17..
-//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 #import <AssetsLibrary/AssetsLibrary.h>
 
-#define NOTIFICATION_ASSET_IMAGE_RETRIEVED @"ASSET_IMG_RETRIEVED"
-
-@interface ViewController : UITableViewController {
+@interface ViewController : UIViewController {
     
 @protected
     
--- clover/ViewController.m
+++ clover/ViewController.m
@@ -1,10 +1,3 @@
-//
-//  ViewController.m
-//  cloverTest2
-//
-//  Created by 강동혁 on 12. 5. 17..
-//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
-//
 
 #import "ViewController.h"
 
@@ -24,14 +17,6 @@
         [newLibray release];
         
         assetsList = [[NSMutableArray alloc] initWithCapacity:0];
-        
-        self.view.backgroundColor = [UIColor whiteColor];
-        
-        self.tableView.allowsSelection = NO;
-        self.tableView.rowHeight = self.view.bounds.size.height;
-        self.tableView.backgroundColor = [UIColor whiteColor];
-        self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-        
     }
     
     return self;
@@ -56,7 +41,6 @@
                 }]; 
             }
             
-            [self.tableView reloadData];
         }
        failureBlock:^(NSError *error) { NSLog(@"failure: %@", [error description]) ; }]; 
   
@@ -73,34 +57,6 @@
     [assetsLibrary release];
     [assetsList release];
     [super dealloc];
-}
-
-#pragma mark UITableViewController delegate
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    
-    return  [self.assetsList count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-        
-        cell.opaque = YES;
-        
-    }
-    
-    ALAsset *asset = [self.assetsList objectAtIndex:indexPath.row];
-    // 이 asset을 이용해서 앨범에 있는 사진이 접근 할 수 있음
-    
-    
-    return cell;
 }
 
 @end
