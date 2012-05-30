0b6094862e8a1e4dab854f256410223ec374424b
dongkang <dongkang0626@daum.net>
2012-05-30 19:31:39 +0900

    Revert "Revert "Revert "Revert "Revert "TimelineViewController 가 NavigationViewController이기 때문에 rootViewControoler역할을 하는 PhotoListViewController 추가"""""
    This reverts commit d9347ad75490126cb3f11c1f3d28c4eb76329dcd.

--- clover/classes/ui/PhotoListViewController.h
+++ clover/classes/ui/PhotoListViewController.h
@@ -3,24 +3,11 @@
 //  clover
 //
 //  Created by 강동혁 on 12. 5. 29..
-//  Copyright (c) 2012년 Rooftop. All rights reserved.
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <AssetsLibrary/AssetsLibrary.h>
-#import "AQGridView.h"
 
-#define VIEW_TITLE @"Photo"
-
-@interface PhotoListViewController : UIViewController <AQGridViewDelegate, AQGridViewDataSource> {
-    
-    ALAssetsLibrary *assetsLibrary;
-    NSMutableArray *assetsList;
-    AQGridView *gridview;
-}
-
-@property (nonatomic, retain) ALAssetsLibrary *assetsLibrary;
-@property (nonatomic, retain) NSMutableArray *assetsList;
-@property (nonatomic, retain) AQGridView *gridView;
+@interface PhotoListViewController : UIViewController
 
 @end
