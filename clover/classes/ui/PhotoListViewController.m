117f24757f360d0c48f608430a95aa20dfe71da2
momolab <hyojinmo@gmail.com>
2012-05-30 09:05:24 +0900

    Revert "Revert "TimelineViewController 가 NavigationViewController이기 때문에 rootViewControoler역할을 하는 PhotoListViewController 추가""
    This reverts commit a279f8705a3ac9ebd6aebaae0c8bc2babf8fd3b5.

--- clover/classes/ui/PhotoListViewController.m
+++ clover/classes/ui/PhotoListViewController.m
@@ -7,26 +7,72 @@
 //
 
 #import "PhotoListViewController.h"
+#import "ImageGridViewCell.h"
 
 @interface PhotoListViewController ()
 
 @end
 
 @implementation PhotoListViewController
+@synthesize assetsLibrary;
+@synthesize assetsList;
+@synthesize gridView;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
+        [self setTitle:VIEW_TITLE];
+        [self setTabBarItem:[[UITabBarItem alloc] initWithTitle:VIEW_TITLE image:nil tag:0]];
+        
+        [self initAssetLib];
+        [self initGridView];
     }
     return self;
+}
+
+- (void) initAssetLib 
+{
+    ALAssetsLibrary *newLibray = [[ALAssetsLibrary alloc] init];
+    self.assetsLibrary = [newLibray retain];
+    [newLibray release];
+}
+
+- (void) initGridView
+{
+     self.assetsList = [[NSMutableArray alloc] initWithCapacity:0];
+     self.gridView = [[AQGridView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+     self.gridView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+     self.gridView.autoresizesSubviews = YES;
+     self.gridView.delegate = self;
+     self.gridView.dataSource = self;
+    
+     [self.view addSubview:gridView];
+     [self.gridView reloadData];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    [self.assetsLibrary enumerateGroupsWithTypes:ALAssetsGroupAll
+     
+                                      usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+                                          
+                                          if (group != nil) { 
+                                              
+                                              [group enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
+                                                  
+                                                  if (result!=nil) { 
+                                                      
+                                                      [self.assetsList addObject:result];
+                                                      [self.gridView reloadData];
+                                                  } 
+                                              }]; 
+                                          }
+                                          
+                                      }
+                                    failureBlock:^(NSError *error) { NSLog(@"failure: %@", [error description]) ; }]; 
 }
 
 - (void)viewDidUnload
@@ -35,9 +81,92 @@
     // Release any retained subviews of the main view.
 }
 
+- (void)dealloc 
+{
+    [gridview release];
+    [assetsLibrary release];
+    [assetsList release];
+    [super dealloc];
+}
+
+- (UIImage *)resizeImage:(UIImage*)image newSize:(CGSize)newSize {
+    CGRect newRect = CGRectIntegral(CGRectMake(0, 0, newSize.width, newSize.height));
+    CGImageRef imageRef = image.CGImage;
+    
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    // Set the quality level to use when rescaling
+    CGContextSetInterpolationQuality(context, kCGInterpolationHigh);
+    CGAffineTransform flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, newSize.height);
+    
+    CGContextConcatCTM(context, flipVertical);  
+    // Draw into the context; this scales the image
+    CGContextDrawImage(context, newRect, imageRef);
+    
+    // Get the resized image from the context and a UIImage
+    CGImageRef newImageRef = CGBitmapContextCreateImage(context);
+    UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
+    
+    CGImageRelease(newImageRef);
+    UIGraphicsEndImageContext();    
+    
+    return newImage;
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
+
+#pragma mark -
+#pragma mark Grid View Data Source
+
+- (NSUInteger) numberOfItemsInGridView: (AQGridView *) aGridView
+{
+    return [self.assetsList count];
+}
+
+- (AQGridViewCell *) gridView: (AQGridView *) aGridView cellForItemAtIndex: (NSUInteger) index
+{
+    static NSString * PlainCellIdentifier = @"ImageCellIdentifier";
+    
+    ALAsset *asset = [self.assetsList objectAtIndex:index];
+    UIImage *image = [[UIImage alloc] initWithCGImage:asset.thumbnail];
+    ImageGridViewCell * cell = (ImageGridViewCell *)[aGridView dequeueReusableCellWithIdentifier:@"ImageCellIdentifier"];
+    if ( cell == nil )
+    {
+        cell = [[ImageGridViewCell alloc] initWithFrame:CGRectMake(0, 0, 75, 75) reuseIdentifier:PlainCellIdentifier];
+    }
+    
+    [cell.imageView setImage:image];
+    
+    return cell;
+}
+
+/*
+ - (CGRect)gridView:(AQGridView *)gridView adjustCellFrame:(CGRect)cellFrame withinGridCellFrame:(CGRect)gridCellFrame {
+ 
+ return CGRectMake(cellFrame.origin.x, cellFrame.origin.y, 60, 60);
+ }
+ */
+
+- (CGSize)portraitGridCellSizeForGridView:(AQGridView *)gridView 
+{
+    return CGSizeMake(80, 80);
+}
+
+- (void)gridView:(AQGridView *)gridView didSelectItemAtIndex:(NSUInteger)index 
+{
+    // 이미지를 클릭하면 발생
+    //ALAsset *asset = [self.assetsList objectAtIndex:index];
+    Class class = NSClassFromString(@"DetailViewController");
+    id detailViewController = [[[class alloc] init] autorelease];
+    
+    if(detailViewController) {
+        [self.navigationController pushViewController:detailViewController animated:YES];
+    }
+}
+
 
 @end
